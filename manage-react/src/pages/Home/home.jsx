930a113f78e8b34eeec07d4b313064ea66183580
jiaorengui <tiny.jiao@aliyun.com>
2019-08-30 18:03:58 +0800

    udpate

--- manage-react/src/pages/Home/home.jsx
+++ manage-react/src/pages/Home/home.jsx
@@ -2,15 +2,21 @@
  * @Author: Tiny 
  * @Date: 2019-07-23 14:42:41 
  * @Last Modified by: tiny.jiao@aliyun.com
- * @Last Modified time: 2019-07-23 14:45:14
+ * @Last Modified time: 2019-08-30 15:53:33
  */
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
 export default class Home extends Component {
+  // static propTypes = {
+  //   name: PropTypes.string,
+  //   passwrod: PropTypes.string
+  // }
   render() {
     return (
       <div>
-        Home page !
+        <h2>Home page !</h2>
+        <p>{  }</p>
       </div>
     )
   }
